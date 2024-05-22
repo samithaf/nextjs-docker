182a925d344998c7aeb56d5a64f28f396bed7180
Samitha Fernando <fernando.samitha@gmail.com>
2024-05-22 15:31:11 +1000

    Update fetcher.js

--- fetcher.js
+++ fetcher.js
@@ -1,3 +1,4 @@
+const fs = require('fs');
 async function download(){
    const response = await fetch("https://gist.githubusercontent.com/samithaf/d0f343c12be293a92a08d2286e94c268/raw/384c8332144a97d22981a7e8cb009ae277d64802/Dockerfile", {
       method: 'GET'
